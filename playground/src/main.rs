0421528d75aa1d888d106805c107ea48c5c88f8e
Sean Myers <spmyers@protonmail.com>
2022-03-24 08:45:26 -0400

    Working on binary search example

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -3,7 +3,12 @@ pub mod staging;
 fn main() {
     let mut random_numbers: Vec<u32> = vec![0u32; 10usize];
     staging::fill_array(&mut random_numbers, Some((0u32, 100u32)));
+    let random_array_value: u32 = staging::select_random_array_value(&random_numbers);
+    random_numbers.sort();
     staging::print_vector_array(&random_numbers);
+    println!("Random number: {random_array_value}");
+    let search_index_result: usize = staging::binary_search(&random_numbers, &random_array_value);
+    println!("Index of random number: {search_index_result}");
 }
 
 #[cfg(test)]
--- playground/src/staging.rs
+++ playground/src/staging.rs
@@ -16,8 +16,43 @@ pub fn fill_array(array: &mut Vec<u32>, random_number_range: Option<(u32, u32)>)
     }
 }
 
-pub fn print_vector_array(array: &Vec<u32>) -> () {
-    for number in array {
-        println!("{number}");
+pub fn select_random_array_value(array: &Vec<u32>) -> u32 {
+    let mut random_number_generator: ThreadRng = rand::thread_rng();
+
+    let random_index = random_number_generator.gen_range(0usize..array.len());
+
+    return array[random_index].clone();
+}
+
+pub fn binary_search(array: &Vec<u32>, target: &u32) -> usize {
+    if array.len() == 0 {
+        return 0usize;
     }
+
+    let mut minimum: usize = 0usize;
+    let mut maximum: usize = array.len() - 1;
+
+    let calculate_middle = |min: usize, max: usize| {
+        return (max + min) / 2;
+    };
+
+    while minimum <= maximum {
+        let middle: usize = calculate_middle(minimum, maximum);
+        if &array[middle] == target {
+            return middle;
+        } else if &array[middle] < target {
+            minimum = middle + 1;
+        } else {
+            maximum = middle - 1;
+        }
+    }
+    0usize
+}
+
+pub fn print_vector_array(array: &Vec<u32>) -> () {
+    print!("Array Values: ");
+    for number in array {
+        print!(" {number} ");
+    }
+    println!("");
 }
